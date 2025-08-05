a2f7062318ff44842c362976c8bab3cee09e4fb1
rachelllxo <rachelveronica309@gmail.com>
2025-08-05 19:13:55 +0530

    Implemented code to create a homepage and new webpage

---
+++ Design_BrowserHistory.cpp
@@ -0,0 +1,42 @@
+/*
+* Filename: Design_BrowserHistory.cpp
+*
+* Authors: Rachel, Afridha Shaheen & Thrisha 
+* 
+* Description: Designed a browser history that can create a Homepage, visit a new page, move backwards and forwards within the browser 
+				using doubly linked list.
+*/
+ 
+#include <iostream>									//header files 
+#include <string>
+using namespace std;
+
+
+struct Webpage {									//structure for a webpage using doubly linked list
+    string data;									//the data part of the doubly linked list that stores the URL			
+    Webpage* prevWebpage;							//the pointer to the previous webpage's address 
+    Webpage* nextWebpage;							//the pointer to the next webpage's address 
+};
+
+Webpage* head = NULL;
+Webpage* NewWebpage = NULL;
+
+string visit_newWebpage(string url) {
+    if (head == NULL) {
+        Webpage* Homepage = new Webpage;		        //creating a new node to store the homepage
+        Homepage->data = url;
+        Homepage->prevWebpage = NULL;			        //the homepage has no previous node
+        Homepage->nextWebpage = NULL; 			        //the homepage's next node is NULL for now 
+        head = Homepage;
+        NewWebpage = Homepage;
+    } else {
+        Webpage* temp = new Webpage;			        //creating a new webpage node that holds the URL
+        temp->data = url;
+        temp->prevWebpage = NewWebpage; 			    //the previous node of the new webpage points to current page
+        temp->nextWebpage = NULL;
+        NewWebpage->nextWebpage = temp;			        //the current page's next points to the new webpage
+        NewWebpage = temp;					            //move current pointer to new webpage
+    }
+    return NewWebpage->data;
+}
+

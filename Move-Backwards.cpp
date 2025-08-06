dc99cb46f5b33676382ebf3eaf60e97845aa3e44
rachelllxo <rachelveronica309@gmail.com>
2025-08-06 20:23:18 +0530

    Added Back function

---
+++ Move-Backwards.cpp
@@ -0,0 +1,14 @@
+
+/*
+ * Function to move backward in the browser history.
+ *
+ * @param - steps - Number of steps to move back.
+ */
+int move_backward(int steps) {
+    while (steps > 0 && NewWebpage->prevWebpage != NULL) {					// As long as steps > 0 and previous page exists
+        NewWebpage = NewWebpage->prevWebpage;								// Move back to previous page
+        steps--;															// Decrease step count
+    }
+    cout << "Current page: " << NewWebpage->data << endl;					// Show current page after moving back
+    return 0;															
+}

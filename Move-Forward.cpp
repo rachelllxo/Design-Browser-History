a4431d33b6912c45314c1b21a164ccd90ecd20e6
rachelllxo <rachelveronica309@gmail.com>
2025-08-06 20:23:44 +0530

    Added Forward function

---
+++ Move-Forward.cpp
@@ -0,0 +1,13 @@
+/*
+ * Function to move forward in the browser history.
+ *
+ * @param - steps - Number of steps to move forward.
+ */
+int move_forward(int steps) {
+    while (steps > 0 && NewWebpage->nextWebpage != NULL) {						// As long as steps > 0 and next page exists
+        NewWebpage = NewWebpage->nextWebpage;									// Move forward to next page
+        steps--;																// Decrease step count
+    }
+    cout << "Current page after going forward: " << NewWebpage->data << endl;	// Show current page after moving forward
+    return 0;															
+}

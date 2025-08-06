ff94bee273b4cb79f12781ff9f44f24bf1bff55f
rachelllxo <rachelveronica309@gmail.com>
2025-08-06 20:24:49 +0530

    Added show function

---
+++ Show-History.cpp
@@ -0,0 +1,17 @@
+/*
+ * Function to display the full browser history from the beginning.
+ */
+int show() {
+    Webpage* temp = head;													// Start from the head of the list
+    cout << "Browser History: ";											// Print heading
+    while (temp != NULL) {													// Loop through all nodes
+        if (temp == NewWebpage) {											// If the node is the current page
+            cout << "[" << temp->data << "] ";								// Highlight it with brackets
+        } else {
+            cout << temp->data << " ";										// Print normally
+        }
+        temp = temp->nextWebpage;											// Move to the next page
+    }
+    cout << endl;															// End the output line
+    return 0;															
+}

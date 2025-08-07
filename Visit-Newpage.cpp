afe89e94207e2f31615f08932a8b4e1bc4a79862
rachelllxo <rachelveronica309@gmail.com>
2025-08-07 09:00:32 +0530

    Added comments

--- Visit-Newpage.cpp
+++ Visit-Newpage.cpp
@@ -8,15 +8,15 @@ int visit_newWebpage(string url) {
     if (head == NULL) {														// If the head pointer is NULL 
         Webpage* Homepage = new Webpage;									// Create a new node for the homepage
         Homepage->data = url;												// Store the URL in the data field
-        Homepage->prevWebpage = NULL;										// Previous pointer is NULL 
-        Homepage->nextWebpage = NULL;										// Next pointer is NULL 
+        Homepage->prevWebpage = nullptr;										// Previous pointer is NULL 
+        Homepage->nextWebpage = nullptr;										// Next pointer is NULL 
         head = Homepage;													// Head now points to homepage
         NewWebpage = Homepage;												// NewWebpage (current page) also points to homepage
     } else {
         Webpage* temp = new Webpage;										// Create a new webpage node for the new URL
         temp->data = url;													// Store the URL in the node's data field
         temp->prevWebpage = NewWebpage;										// Set the previous pointer to current page
-        temp->nextWebpage = NULL;											// Set next pointer to NULL (end of list)
+        temp->nextWebpage = nullptr;											// Set next pointer to NULL (end of list)
         NewWebpage->nextWebpage = temp;										// Current page's next pointer updated to new node
         NewWebpage = temp;													// Move current page pointer to the new page
     }
